b00e6a44b6731e6c8006dcb908b36cc25f71356c
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-14 17:32:56 +0800

    Create MainProgram.java

---
+++ part4/Part04_16.PaymentCard/MainProgram.java
@@ -0,0 +1,8 @@
+public class MainProgram {
+    public static void main(String[] args) {
+        PaymentCard card = new PaymentCard(10);
+        System.out.println("Paul: " + card);
+        card.addMoney(-15);
+        System.out.println("Paul: " + card);
+    }
+}
