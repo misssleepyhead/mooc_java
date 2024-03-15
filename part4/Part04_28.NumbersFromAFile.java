372c3ca94f4de42eac339e1c4682c3a670435a2f
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-15 14:31:57 +0800

    Create Part04_28.NumbersFromAFile.java

---
+++ part4/Part04_28.NumbersFromAFile.java
@@ -0,0 +1,33 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class NumbersFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File? ");
+        String file = scanner.nextLine();
+        System.out.print("Lower bound? ");
+        int lowerBound = Integer.valueOf(scanner.nextLine());
+        System.out.print("Upper bound? ");
+        int upperBound = Integer.valueOf(scanner.nextLine());
+        int count = 0;
+        try ( Scanner scanner2 = new Scanner(Paths.get(file))) {
+            while (scanner2.hasNextLine()) {
+                int number = Integer.valueOf(scanner2.nextLine());
+                if (lowerBound <= number && number <= upperBound) {
+                    count++;
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+        System.out.println("Numbers: "+count);
+
+    }
+
+}
