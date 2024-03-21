def8af7e8c4f5267b6fc3cefe03f0299142b5d75
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-21 15:20:34 +0800

    Create GradeRegister.java

---
+++ part 6 /Part06_11.Averages/GradeRegister.java
@@ -0,0 +1,80 @@
+
+import java.util.ArrayList;
+
+public class GradeRegister {
+
+    private ArrayList<Integer> grades;
+    private ArrayList<Integer> points;
+
+    public GradeRegister() {
+        this.grades = new ArrayList<>();
+        this.points = new ArrayList<>();
+    }
+
+    public void addGradeBasedOnPoints(int points) {
+        this.points.add(points);
+        this.grades.add(pointsToGrade(points));
+    }
+
+    public int numberOfGrades(int grade) {
+        int count = 0;
+        for (int received : this.grades) {
+            if (received == grade) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int pointsToGrade(int points) {
+
+        int grade = 0;
+        if (points < 50) {
+            grade = 0;
+        } else if (points < 60) {
+            grade = 1;
+        } else if (points < 70) {
+            grade = 2;
+        } else if (points < 80) {
+            grade = 3;
+        } else if (points < 90) {
+            grade = 4;
+        } else {
+            grade = 5;
+        }
+
+        return grade;
+    }
+
+    public double averageOfGrades() {
+        double avg = 0;
+        if (grades.size() == 0) {
+            avg = -1;
+
+        } else {
+            for (int grade : grades) {
+                avg += grade;
+
+            }
+            avg = avg * 1.0 / grades.size();
+        }
+        return avg;
+    }
+
+    public double averageOfPoints() {
+        double avg = 0;
+        if (points.size() == 0) {
+            avg = -1;
+
+        } else {
+            for (int point : points) {
+                avg += point;
+
+            }
+            avg = avg * 1.0 / points.size();
+        }
+        return avg;
+
+    }
+}
