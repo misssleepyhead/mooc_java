8dd57897dda2af51c76081ddfdd2e06ef7a6f72a
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-26 17:05:08 +0800

    Create Main.java

---
+++ part 7/ part07-Part07_06.GradeStatistics/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Points points = new Points();
+        UserInterface ui = new UserInterface(points, scanner);
+        ui.start();
+    }
+}
