c7d76083229fd51600b020d027f48a48e5a00dc8
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-26 12:16:13 +0800

    Create LiquidContainers.java

---
+++ part 7/Part07_01.LiquidContainers/LiquidContainers.java
@@ -0,0 +1,69 @@
+
+import java.util.Scanner;
+
+public class LiquidContainers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
+
+        while (true) {
+            System.out.print("> ");
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
+
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add")) {
+
+                if (amount > 0 && (amount + first) <= 100) {
+                    first += amount;
+
+                } else if ((amount + first) > 100) {
+                    first = 100;
+                }
+            } else if (command.equals("move")) {
+                if (amount > 0) {
+                    if (first >= amount) {
+                        first -= amount;
+                        if ((second + amount) <= 100) {
+                            second += amount;
+                        } else {
+                            second = 100;
+
+                        }
+                    } else if (first < amount) {
+                        second += first;
+                        first = 0;
+                        if ((second + amount) >= 100) {
+                            second = 100;
+
+                        }
+
+                    }
+
+                }
+            } else if (command.equals("remove")) {
+                if (amount > 0) {
+                    if (amount <= second) {
+                        second -= amount;
+
+                    } else if (amount > second) {
+                        second = 0;
+
+                    }
+                }
+            }
+
+        }
+    }
+}
