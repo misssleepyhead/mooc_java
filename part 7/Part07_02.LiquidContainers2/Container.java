a1d7f42d19862c43c7a7343e97bffaaa537fe81a
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-03-28 10:45:51 +0800

    Create Container.java

---
+++ part 7/Part07_02.LiquidContainers2/Container.java
@@ -0,0 +1,41 @@
+public class Container {
+ 
+    private int contain;
+ 
+    public Container() {
+        this.contain = 0;
+    }
+ 
+    public int contains() {
+        return contain;
+ 
+    }
+ 
+    public void add(int amount) {
+        if (amount > 0) {
+            this.contain += amount;
+            if (this.contain > 100) {
+                this.contain = 100;
+            }
+        }
+ 
+    }
+ 
+    public void remove(int amount) {
+        if (amount > 0) {
+            this.contain -= amount;
+            if (this.contain < 0) {
+                this.contain = 0;
+            }
+ 
+        }
+ 
+    }
+ 
+    public String toString() {
+ 
+        return this.contains() + "/100";
+ 
+    }
+ 
+}
