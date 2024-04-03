8228c4f26efeeefc4046d0b46d36b67142fc5584
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-03 18:59:53 +0800

    Create  CustomTacoBox.java

---
+++ Part 9 /Part09_05.TacoBoxes/CustomTacoBox.java
@@ -0,0 +1,23 @@
+
+public class CustomTacoBox implements TacoBox {
+
+    private int tacos;
+
+    public CustomTacoBox(int tacos) {
+        this.tacos = tacos;
+    }
+
+    @Override
+    public int tacosRemaining() {
+        return this.tacos;
+    }
+
+    @Override
+    public void eat() {
+        this.tacos--;
+        if (this.tacos < 0) {
+            this.tacos = 0;
+        }
+    }
+
+}
