905d4cdd1a092b5b71ce4b4a072e84bf13ffc2f4
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-08 17:42:16 +0800

    Create Cat.java

---
+++ Part 9 /Part09_12.Animals/Cat.java
@@ -0,0 +1,28 @@
+
+public class Cat extends Animal implements NoiseCapable{
+    private String name;
+
+    public Cat() {
+        super("Cat");
+    }
+
+    public Cat(String name) {
+        super(name);
+        
+    }
+    
+    public void purr(){
+        System.out.println(this.getName()+" purrs");
+    
+    }
+
+
+
+
+    @Override
+    public void makeNoise() {
+        purr();
+    }
+    
+    
+}
