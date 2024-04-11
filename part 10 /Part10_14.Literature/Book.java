793b8218d08457d2e26404b61ede60ebce8cadb9
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-11 15:59:08 +0800

    Create Book.java

---
+++ part 10 /Part10_14.Literature/Book.java
@@ -0,0 +1,26 @@
+
+public class Book {
+    private String title;
+    private int age;
+
+    public Book(String title, int age) {
+        this.title = title;
+        this.age = age;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+
+    @Override
+    public String toString() {
+        return this.title+" (recommended for "+this.age+" year-olds or older)";
+    }
+    
+    
+}
