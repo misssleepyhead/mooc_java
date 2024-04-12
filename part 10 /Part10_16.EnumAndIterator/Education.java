3ef0ff0e24b2389c4e0d5e3448f374ceb83f118a
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-12 11:48:43 +0800

    Create Education.java

---
+++ part 10 /Part10_16.EnumAndIterator/Education.java
@@ -0,0 +1,18 @@
+
+public enum Education {
+    PHD("Doctoral degree"),
+    MA("Masters degree"),
+    BA("Bachelors degree"),
+    HS("High School diploma");
+    
+    private String degree;
+
+    private Education(String degree) {
+        this.degree = degree;
+    }
+    
+    
+   
+    
+    
+}
