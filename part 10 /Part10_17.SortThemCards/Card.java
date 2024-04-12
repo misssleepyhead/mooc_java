0f13fd24f9986bd8c55059a9e1800e795835b393
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-12 16:43:58 +0800

    Create Card.java

---
+++ part 10 /Part10_17.SortThemCards/Card.java
@@ -0,0 +1,70 @@
+
+import java.util.Comparator;
+
+
+public class Card implements Comparable<Card> {
+
+    private int value;
+    private Suit suit;
+
+    public Card(int value, Suit suit) {
+        if (value < 2 || value > 14) {
+            throw new IllegalArgumentException("Card value must be in range 2...14.");
+        }
+
+        this.value = value;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        String cardValue = "" + value;
+        if (value == 11) {
+            cardValue = "J";
+        } else if (value == 12) {
+            cardValue = "Q";
+        } else if (value == 13) {
+            cardValue = "K";
+        } else if (value == 14) {
+            cardValue = "A";
+        }
+
+        return suit + " " + cardValue;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public int compareTo(Card compared) {
+
+        if (this.getValue() == compared.getValue()) {
+            if (this.getSuit() == compared.getSuit()) {
+                return 0;
+            } else {
+                return this.getSuit().compareTo(compared.getSuit());
+            }
+
+        } else if (this.getValue() > compared.getValue()) {
+            return 1;
+        } else {
+            return -1;
+
+        }
+    }
+
+//    @Override
+//    public int compareTo(Card arg0) {
+//         Comparator<Card> comparator = Comparator
+//                .comparing(Card::getValue)
+//                .thenComparing(Card::getSuit)
+//         
+//    }
+    
+    
+}
