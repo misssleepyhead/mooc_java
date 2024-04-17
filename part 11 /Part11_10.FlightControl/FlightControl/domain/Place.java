45371ae2bcf6bd09ef5edf4f1392620cf668a6c5
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-17 17:13:16 +0800

    Create Place.java

---
+++ part 11 /Part11_10.FlightControl/FlightControl/domain/Place.java
@@ -0,0 +1,18 @@
+
+package FlightControl.domain;
+
+
+public class Place {
+    private String ID;
+
+    public Place(String ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID;
+    }
+    
+    
+}
