9234d43cb3bcd9293a986c69689c59091d660953
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-23 17:54:15 +0800

    Create Program.java

---
+++ part 12/Part12_06.Numbers/Program.java
@@ -0,0 +1,21 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("How many random numbers should be printed?");
+        int times = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < times; i++) {
+            int num = rand.nextInt(11);
+            System.out.println(num);
+
+        }
+
+    }
+
+}
