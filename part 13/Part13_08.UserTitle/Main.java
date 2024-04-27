0bc9ab4ebb6789035afc02622b79625833004735
Ariel <108454984+misssleepyhead@users.noreply.github.com>
2024-04-27 16:53:55 +0800

    Create Main.java

---
+++ part 13/Part13_08.UserTitle/Main.java
@@ -0,0 +1,15 @@
+package title;
+
+import javafx.application.Application;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Application.launch(UserTitle.class, "--title=Hi it's me");
+        System.out.println("Hello world!");
+
+    }
+
+}
